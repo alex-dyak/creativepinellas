11aaa58e4072ebc0db9c139097917ad72f2885f8
Alexey Minaev <aminaev@corp.web4pro.com.ua>
2016-08-04 17:43:05 +0200

    28220 style mobile nav toggler and header for mobile bp

--- static-html/js/custom/app.js
+++ static-html/js/custom/app.js
@@ -4,5 +4,15 @@
 
     $(document).foundation();
 
+    var $offCanvas = $(".off-canvas"),
+      $mobileNavToggler = $('.js-mobileNavToggle');
+
+    $offCanvas.on("opened.zf.offcanvas", function(e) {
+      $mobileNavToggler.addClass('is-open');
+    });
+    $offCanvas.on("closed.zf.offcanvas", function(e) {
+      $mobileNavToggler.removeClass('is-open');
+    });
+
   });
 })();
