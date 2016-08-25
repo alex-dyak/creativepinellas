2afe2c02cf30fd8c014f2807af52d27e7705ba0b
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2016-08-25 14:07:56 +0300

    28496 fix size of description.

--- wp-content/themes/w4ptheme/functions.php
+++ wp-content/themes/w4ptheme/functions.php
@@ -401,3 +401,23 @@ function get_related_events($location_id){
         return $replace = EM_Events::get($args);
     }
 }
+
+
+function the_excerpt_max_charlength( $charlength ){
+	$excerpt = get_the_excerpt();
+	$charlength++;
+
+	if ( mb_strlen( $excerpt ) > $charlength ) {
+		$subex = mb_substr( $excerpt, 0, $charlength - 5 );
+		$exwords = explode( ' ', $subex );
+		$excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
+		if ( $excut < 0 ) {
+			echo mb_substr( $subex, 0, $excut );
+		} else {
+			echo $subex;
+		}
+		echo '(...)';
+	} else {
+		echo $excerpt;
+	}
+}
